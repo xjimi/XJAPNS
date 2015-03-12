f444d6f344bc7247f1bfa25f5eefa3cf0547af8e
XJIMI <xjimi@PORTALteki-iMac.local>
2015-03-12 16:54:41 +0800

    Initial Commit

---
+++ XJAPNS_Demo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  XJAPNS_Demo
+//
+//  Created by XJIMI on 2015/3/12.
+//  Copyright (c) 2015年 XJIMI. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ XJAPNS_DemoTests/XJAPNS_DemoTests.m
@@ -0,0 +1,40 @@
+//
+//  XJAPNS_DemoTests.m
+//  XJAPNS_DemoTests
+//
+//  Created by XJIMI on 2015/3/12.
+//  Copyright (c) 2015年 XJIMI. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface XJAPNS_DemoTests : XCTestCase
+
+@end
+
+@implementation XJAPNS_DemoTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
